de3bf106045a0496b8235831e28c633ec2242b88
naresh <naresh.tech20@gmail.com>
2024-01-24 20:02:43 +0000

    first commit

---
+++ messages/read_msg.go
@@ -0,0 +1,35 @@
+package messages
+
+import (
+	"fmt"
+	"os"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+
+var messageHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+}
+
+func Subscribe() {
+
+	//go func() {
+	 opts := mqtt.NewClientOptions().AddBroker("tcp://edgex-mqtt-broker:1883")
+	 opts.SetDefaultPublishHandler(messageHandler)
+	 client := mqtt.NewClient(opts)
+	 token := client.Connect()
+	 token.Wait()
+
+	 if token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	 }
+
+	 token = client.Subscribe("topic", 0, nil)
+	 token.Wait()
+	 fmt.Printf("Successfully subscribed to topic: %s\n", "topic")
+
+	 select {} // block forever
+ 	//}()  
+}
